d0d5f733de6c16402df64592678b6537f56a419a
Anish Hazra <anishhazra108@gmail.com>
2024-01-21 12:31:31 +0530

    Time: 6 ms (53.47%), Space: 6.9 MB (11.03%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int fibo(int n){
+        //base case
+        if(n == 0 or n == 1){
+            return n;
+        }
+        int f1 = fibo(n-1);
+        int f2 = fibo(n-2);
+        return f1+f2;
+    }
+    int fib(int n) {
+        return fibo(n);
+    }
+};
